50a66a41e44312e144761dbf466d84fe2214c2cc
Ian Hutchinson <ianhutch90@gmail.com>
2021-09-15 22:07:19 +0100

    Using Collection Classes

--- src/main/kotlin/ org/wit/placemark/console/models/PlacemarkModel.kt
+++ src/main/kotlin/ org/wit/placemark/console/models/PlacemarkModel.kt
@@ -1,4 +1,10 @@
 package ` org`.wit.placemark.console.models
 
-data class PlacemarkModel(var title: String = "",
-                          var description: String = "")
+data class PlacemarkModel(
+    var id: Long = 0,
+    var title: String = "",
+    var description: String = "") {
+
+
+
+}
--- src/main/kotlin/org/wit/placemark/console/main/main.kt
+++ src/main/kotlin/org/wit/placemark/console/main/main.kt
@@ -5,9 +5,9 @@ import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
-var title =""
-var description = ""
-var placemark = PlacemarkModel()
+//manage multiple 'Placemark' objects
+var placemarks = ArrayList<PlacemarkModel>()
+
 
 fun main(args: Array<String>) {
     logger.info { "Launching Placemark Console App" }
@@ -21,6 +21,8 @@ fun main(args: Array<String>) {
             1 -> addPlacemark()
             2 -> updatePlacemark()
             3 -> listPlacemarks()
+            4 -> searchPlacemark()
+            -99 -> dummyData()
             -1 -> println("Exiting App")
             else -> println("Invalid Option")
         }
@@ -38,6 +40,7 @@ fun menu() : Int {
     println(" 1. Add Placemark")
     println(" 2. Update Placemark")
     println(" 3. List All Placemarks")
+    println(" 4. Search Placemarks")
     println("-1. Exit")
     println()
     print("Enter an integer : ")
@@ -50,29 +53,82 @@ fun menu() : Int {
 }
 
 fun addPlacemark(){
-
+    var aPlacemark = PlacemarkModel()
     println("Add Placemark")
     println()
     print("Enter a Title: ")
-    placemark.title = readLine()!!
+    aPlacemark.title = readLine()!!
     print("Enter a Description: ")
-    placemark.description = readLine()!!
-    println("You entered [" + placemark.title + "] for title " +
-            "and [ " + placemark.description + "] for description")
+    aPlacemark.description = readLine()!!
 
+    if (aPlacemark.title.isNotEmpty() && aPlacemark.description.isNotEmpty()) {
+        placemarks.add(aPlacemark.copy())
+        logger.info("Placemark Added : [ $aPlacemark ]")
+    }
+    else
+        logger.info("Placemark Not Added")
 }
 
 fun updatePlacemark() {
     println("Update Placemark")
     println()
-    print("Enter a new Title for [ " + placemark.title + " ] : ")
-    placemark.title = readLine()!!
-    print("Enter a new Description for [ " + placemark.description + " ] : ")
-    placemark.description = readLine()!!
-    println("You updated [ " + placemark.title + " ] for title " +
-            "and [ " + placemark.description + " ] for description")
+    listPlacemarks()
+    var searchId = getId()
+    val aPlacemark = search(searchId)
+
+    if(aPlacemark != null) {
+        print("Enter a new Title for [ " + aPlacemark.title + " ] : ")
+        aPlacemark.title = readLine()!!
+        print("Enter a new Description for [ " + aPlacemark.description + " ] : ")
+        aPlacemark.description = readLine()!!
+        println(
+            "You updated [ " + aPlacemark.title + " ] for title " +
+                    "and [ " + aPlacemark.description + " ] for description"
+        )
+    }
+    else
+        println("Placemark Not Updated...")
 }
 
 fun listPlacemarks() {
-    println("You Chose List All Placemarks")
+    println("List All Placemarks")
+    println()
+    placemarks.forEach { logger.info("${it}") }
+    println()
+}
+
+fun searchPlacemark() {
+    var searchId = getId()
+    // create Placemark object here and assign,
+    val aPlacemark = search(searchId)
+    // based on 'searchId' value passed to 'search()'
+    if(aPlacemark != null)
+        println("Placemark Details [ $aPlacemark ]")
+    else
+        println("Placemark Not Found...")
+}
+
+fun getId() : Long {
+    var strId : String? // String to hold user input
+    var searchId : Long // Long to hold converted Id
+
+    print("Enter id to Search/Update : ")
+    strId = readLine()!!
+    searchId = if (strId.toLongOrNull() != null && !strId.isEmpty())
+        strId.toLong()
+    else
+        -9
+    return searchId
+}
+
+fun search(id: Long) : PlacemarkModel? {
+    var foundPlacemark: PlacemarkModel? = placemarks.find { p -> p.id == id }
+    return foundPlacemark
+}
+
+
+fun dummyData() {
+    placemarks.add(PlacemarkModel(1, "New York New York", "So Good They Named It Twice"))
+    placemarks.add(PlacemarkModel(2, "Ring of Kerry", "Some place in the Kingdom"))
+    placemarks.add(PlacemarkModel(3, "Waterford City", "You get great Blaas Here!!"))
 }
